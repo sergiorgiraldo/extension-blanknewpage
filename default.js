03089a6e2e28e85e3e4a6048d3849a9dc95a42ab
Sergio Giraldo <sergio@giraldo.com.br>
2023-11-18 10:19:46 +0100

    feat: make the images random:: by sergio giraldo @ 20231118T1019CET, gpg signed

---
+++ default.js
@@ -0,0 +1,9 @@
+    const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg", "15.jpg", "16.jpg"];
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomImage = images[randomIndex];
+    var element = document.querySelector("html");
+    element.style.backgroundImage = "url(" + randomImage + ")";
+  	element.style.backgroundRepeat = "no-repeat";
+	element.style.backgroundPosition = "center center";
+	element.style.backgroundAttachment = "fixed";  
+	element.style.backgroundSize = "cover";
