1387bdf8073d0b03ce6e66dcd65a3f69209a969f
Sergio Giraldo <sergio@giraldo.com.br>
2024-02-25 22:50:09 +0100

    feat: clock and weather:: by sergio giraldo @ 20240225T2250CET, gpg signed

--- default.js
+++ default.js
@@ -7,3 +7,36 @@
 	element.style.backgroundPosition = "center center";
 	element.style.backgroundAttachment = "fixed";  
 	element.style.backgroundSize = "cover";
+
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+        document.getElementById("wthru").innerHTML = this.responseText;
+    }
+    };
+    xhr.open("GET", "https://wttr.in/Utrecht?format=%l:+%c+%t(%f)");
+    xhr.send();
+
+    const xhr2 = new XMLHttpRequest();
+    xhr2.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+        document.getElementById("wthra").innerHTML = this.responseText;
+    }
+    };
+    xhr2.open("GET", "https://wttr.in/Amsterdam?format=%l:+%c+%t(%f)");
+    xhr2.send();
+
+    function updateClock() {
+        const now = new Date();
+        let hours = String(now.getHours()).padStart(2, '0');
+        let minutes = String(now.getMinutes()).padStart(2, '0');
+        let seconds = String(now.getSeconds()).padStart(2, '0');
+        
+        document.getElementById('clock').innerHTML = `${hours}:${minutes}:${seconds}<br/>${now.toLocaleDateString()} `;
+        
+        setTimeout(updateClock, 1000); // Update every second
+    }
+    
+    window.onload = () => {
+        updateClock();
+    };
